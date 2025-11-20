b6a2c5dc69257f27dde58b7eb8aa0ad8c1c3d90c
Chris Maas <gitlab@cmaas.de>
2025-11-20 12:55:59 +0100

    styling

--- src/views/password-reset.ts
+++ src/views/password-reset.ts
@@ -6,7 +6,7 @@ import { MainReduced } from './main.js';
 export function PasswordResetRequestForm({ values, errors }: FormValues): HtmlEscapedString | Promise<HtmlEscapedString> {
 	return MainReduced(html`
 		<h1>Forgot Password?</h1>
-		<form method="POST" action="/reset-password">
+		<form method="POST" action="/reset-password" class="stack">
 			${errors.length > 0 ? html`<ul class="error" style="color:red;">${errors.map(err => html`<li>${err.message}</li>`)}</ul>` : ''}
 			<input type="text" name="email" placeholder="Email" value="${values.email || ""}" aria-invalid="${!!errors.find(e => e.field === 'email')}"/>
 			<button type="submit">Send password reset email</button>
@@ -26,10 +26,10 @@ export function PasswordResetRequestSucess(): HtmlEscapedString | Promise<HtmlEs
 export function NewPasswordForm({ values, errors }: FormValues): HtmlEscapedString | Promise<HtmlEscapedString> {
 	return MainReduced(html`
 		<h1>Enter New Password</h1>
-		<form method="POST" action="/set-password">
+		<form method="POST" action="/set-password" class="stack">
 			${errors.length > 0 ? html`<ul class="error" style="color:red;">${errors.map(err => html`<li>${err.message}</li>`)}</ul>` : ''}
-			<fieldset>
-				<input type="text" name="password" value="" placeholder="New Password" aria-invalid="${!!errors.find(e => e.field === 'password')}"/>
+			<fieldset class="stack">
+				<input type="password" name="password" value="" placeholder="New Password" aria-invalid="${!!errors.find(e => e.field === 'password')}"/>
 				<label>
 	    			<input name="show-password" type="checkbox" role="switch" onchange="document.getElementsByName('password')[0].type = this.checked ? 'text' : 'password'"> Show password
   				</label>
